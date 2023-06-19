491e486c4d0872a56ae56732d0f8b0c88026eec0
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-19 11:24:44 -0700

    build checkbox functions in js'

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,19 @@ Pizza.prototype.chooseSize = function () {
   this.size;
 };
 
-Pizza.prototype.chooseToppings = function () {
-
-};
+Pizza.prototype.chooseToppings = function () {};
 
 // UI Logic
 
-let myPizza = new Pizza
+let myPizza = new Pizza();
 
-function getToppings (myPizza) {
-    let toppings = document.querySelectorAll("form#pizza-toppings").checked;
-    
+function getToppings(myPizza) {
+  let toppings = document.forms[0];
+  for (i = 0; i < toppings.length; i++) {
+    if (toppings[i].checked) {
+      txt = txt + toppings[i].value + " ";
+    }
+  }
+  document.getElementById("pizza-order").value =
+    "You ordered a pizza with: " + txt;
 }

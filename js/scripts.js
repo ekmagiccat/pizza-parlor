2083d1006cf5ba21eb43884471371a28d534a73d
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-19 13:11:09 -0700

    add to checkbox form

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,29 @@
 // Business Logic
-function Pizza(size, toppings) {
+function Pizza(size, toppings, orderTotal) {
   this.toppings = toppings;
   this.size = size;
+  this.orderTotal = orderTotal;
 }
 
 Pizza.prototype.chooseSize = function () {
-  this.size;
+  return this.size;
 };
 
-Pizza.prototype.chooseToppings = function () {};
+Pizza.prototype.chooseToppings = function () {
+  return this.toppings;
+};
 
 // UI Logic
 
 let myPizza = new Pizza();
 
-function getToppings(myPizza) {
-  let toppings = document.forms[0];
+function getToppings() {
+  let toppings = document.getElementById("pizza-toppings").value;
   for (i = 0; i < toppings.length; i++) {
     if (toppings[i].checked) {
       txt = txt + toppings[i].value + " ";
     }
   }
-  document.getElementById("pizza-order").value =
-    "You ordered a pizza with: " + txt;
+  return (document.getElementById("pizza-order").value =
+    "You ordered a pizza with: " + txt);
 }

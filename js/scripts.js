49f9773bffd432f6f818b14086fe5b3d6a873b18
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-19 19:20:58 -0700

    update css styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,61 @@ function Pizza(size, toppings, orderTotal) {
 }
 
 Pizza.prototype.chooseSize = function () {
-  document.querySelectorAll("input[name=size]:checked") = this.size;
+  this.size = document.querySelectorAll("input[name=size]:checked");
 };
 
 Pizza.prototype.chooseToppings = function () {
-  return this.toppings;
+  this.toppings = document.querySelectorAll("input[name=toppings]:checked");
 };
 
 Pizza.prototype.orderTotal = function () {
-  return this.toppings + this.size;
+  let total = 0;
+  const toppings = document.querySelectorAll("input[name=toppings]:checked");
+
+  toppings.forEach(function (topping) {
+    const toppingPrice = parseFloat(topping.getAttribute("data-price"));
+    total += toppingPrice;
+  });
 };
+
+/* 
+
+      let toppingsPrice = document.querySelectorAll("input[name=toppings]:checked").getAttribute("data-price").value;
+    const toppingsPriceArray = Array.from(toppingsPrice);
+    toppingsPriceArray.forEach(function(number) {
+  return number + total;
+});
+
+ window.totalIt= function(name) {
+  if(name == "product"){
+
+  var input = document.getElementsByName("product");
+  var total = 0;
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].checked) {
+          total += 1;
+        }
+      }
+  if(total>=3){ total1 =  (total*29).toFixed(2);}
+  else{total1 =  (total*39).toFixed(2);}
+  }
+
+if(name == "dessert"){
+
+  var input = document.getElementsByName("dessert");
+  var total = 0;
+  for (var i = 0; i < input.length; i++) {
+    if (input[i].checked) {
+      total += parseFloat(input[i].value);
+    }
+  }
+
+  total2 =  total.toFixed(2);
+  }
+  grndTotal = parseInt(total2) + parseInt(total1);
+  document.getElementById("total").value = "$"+grndTotal ;
+
+} */
 
 // UI Logic
 
@@ -42,7 +87,7 @@ function getSize(event) {
     document.body.append(paragraph);
   });
 }
-  
+
 /* function calculateOrderTotal(event) {
     let price = document.querySelectorAll("input[data-price]").value;
     const size = document.querySelectorAll("input[name=size]:checked");
@@ -61,14 +106,13 @@ function getSize(event) {
 
     return totalPrice;
   }
-*/ 
+*/
 window.addEventListener("load", function (event) {
-     event.preventDefault();
+  event.preventDefault();
   document
     .getElementById("pizzaSelections")
     .addEventListener("submit", function (event) {
       getSize(event);
       getToppings(event);
     });
-   
 });
